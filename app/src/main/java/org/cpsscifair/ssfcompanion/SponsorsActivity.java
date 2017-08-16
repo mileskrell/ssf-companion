813f8fb376d9b25b3a84cbc00b0cb272ae6dcd00
Miles Krell <repos@mileskrell.com>
2017-08-15 20:49:58 -0500

    Let the user click on the sponsor logos to visit their respective sites.

--- app/src/main/java/org/cpsscifair/ssfcompanion/SponsorsActivity.java
+++ app/src/main/java/org/cpsscifair/ssfcompanion/SponsorsActivity.java
@@ -1,8 +1,11 @@
 package org.cpsscifair.ssfcompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class SponsorsActivity extends AppCompatActivity {
 
@@ -23,5 +26,37 @@ public class SponsorsActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void viewSsfIncSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://cpsstudentsciencefair.squarespace.com/")));
+    }
+
+    public void viewCpsSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://cps.edu/")));
+    }
+
+    public void viewMsiSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.msichicago.org/")));
+    }
+
+    public void viewTakedaSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.takeda.com/")));
+    }
+
+    public void viewBpSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bp.com/")));
+    }
+
+    public void viewComedSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.comed.com/")));
+    }
+
+    public void viewMotorolaSolutionsFoundationSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.motorolasolutions.com/en_us/about/company-overview/corporate-responsibility/motorola-solutions-foundation.html")));
+    }
+
+    public void viewPeoplesGasSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.peoplesgasdelivery.com/")));
     }
 }
