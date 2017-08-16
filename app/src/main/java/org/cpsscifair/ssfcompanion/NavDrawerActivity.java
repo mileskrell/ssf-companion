b14e886bdd57f446941ce17713f68c9dc7d4b82e
Miles Krell <repos@mileskrell.com>
2017-08-16 14:54:00 -0500

    Removed "About our sponsors" activity and put its contents in NavDrawerActivity. Also, replaced SSF logo with a smaller one.

--- app/src/main/java/org/cpsscifair/ssfcompanion/NavDrawerActivity.java
+++ app/src/main/java/org/cpsscifair/ssfcompanion/NavDrawerActivity.java
@@ -1,6 +1,7 @@
 package org.cpsscifair.ssfcompanion;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -10,6 +11,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,14 +45,40 @@ public class NavDrawerActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.nav_about_our_sponsors:
-                startActivity(new Intent(this, AboutOurSponsorsActivity.class));
-                break;
-        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void viewSsfIncSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://cpsstudentsciencefair.squarespace.com/")));
+    }
+
+    public void viewCpsSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://cps.edu/")));
+    }
+
+    public void viewMsiSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.msichicago.org/")));
+    }
+
+    public void viewTakedaSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.takeda.com/")));
+    }
+
+    public void viewBpSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bp.com/")));
+    }
+
+    public void viewComedSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.comed.com/")));
+    }
+
+    public void viewMotorolaSolutionsFoundationSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.motorolasolutions.com/en_us/about/company-overview/corporate-responsibility/motorola-solutions-foundation.html")));
+    }
+
+    public void viewPeoplesGasSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.peoplesgasdelivery.com/")));
     }
 }
