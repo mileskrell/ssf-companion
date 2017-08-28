f7c2ba99f32bc22210e3d2468701aaf6e51f03fa
Miles Krell <noreply@mileskrell.com>
2017-08-27 20:28:50 -0500

    Added content to ChecklistFragment. The user can now add and check/uncheck items (which gives them a strike-through effect). The list contains four sample items which provide instructions on using the checklist.
    Added CardView support library as a dependency.

---
+++ app/src/main/java/org/cpsscifair/ssfcompanion/ChecklistAdapter.java
@@ -0,0 +1,155 @@
+package org.cpsscifair.ssfcompanion;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+class ChecklistAdapter extends RecyclerView.Adapter<ChecklistAdapter.ViewHolder> {
+
+    private static final String CHECKLIST_ITEMS = "checklist_items";
+
+    private ArrayList<String> checklistItems;
+    private Context context;
+
+    ChecklistAdapter(Context context) {
+
+        String[] defaultItemsArray = context.getResources().getStringArray(R.array.default_checklist_items);
+        HashSet<String> defaultItemsHashSet = new HashSet<>(Arrays.asList(defaultItemsArray));
+
+        HashSet<String> checklistItemsHashSet
+                = new HashSet<>(PreferenceManager.getDefaultSharedPreferences(context)
+                .getStringSet(CHECKLIST_ITEMS, defaultItemsHashSet));
+
+        checklistItems = new ArrayList<>(checklistItemsHashSet);
+
+        sortItems();
+        this.context = context;
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox checkBox;
+        TextView textView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checklist_item_check_box);
+            textView = (TextView) itemView.findViewById(R.id.checklist_item_text_view);
+        }
+    }
+
+    @Override
+    public ChecklistAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.checklist_item, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ChecklistAdapter.ViewHolder holder, final int position) {
+        // First, we figure out whether this item should be checked
+        String item = checklistItems.get(position);
+        int firstCommaPosition = getFirstCommaPosition(item);
+
+        if (item.charAt(firstCommaPosition + 2) == '1') {
+            holder.checkBox.setChecked(true);
+            // Add strike-through to TextView
+            // See https://stackoverflow.com/a/9786629
+            holder.textView.setPaintFlags(holder.textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        // Now, we add the text for the item
+        item = item.substring(firstCommaPosition + 5);
+        holder.textView.setText(item);
+
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Add/remove strike-through
+                // See https://stackoverflow.com/a/9786629
+                if (isChecked) {
+                    holder.textView.setPaintFlags(holder.textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    holder.textView.setPaintFlags(holder.textView.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                }
+
+                // Save new checkbox state
+
+                String changedItem = checklistItems.get(holder.getAdapterPosition());
+
+                int firstCommaPosition = getFirstCommaPosition(changedItem);
+
+                // Includes everything before the boolean
+                String finalItem = changedItem.substring(0, firstCommaPosition + 2);
+
+                if (isChecked) {
+                    finalItem += "1" + changedItem.substring(finalItem.length() + 1);
+                } else {
+                    finalItem += "0" + changedItem.substring(finalItem.length() + 1);
+                }
+
+                checklistItems.set(holder.getAdapterPosition(), finalItem);
+
+                saveItems();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return checklistItems.size();
+    }
+
+    private int getFirstCommaPosition(String itemContainingComma) {
+        int commaPosition = 0;
+        // Increment until we get to the first comma
+        while (itemContainingComma.charAt(commaPosition) != ',') {
+            commaPosition ++;
+        }
+        return commaPosition;
+    }
+
+    private void sortItems() {
+        int x = 1;
+        ArrayList<String> sortedItems = new ArrayList<>();
+
+        for (int i = 0; i < checklistItems.size(); i ++) {
+            for (String item : checklistItems) {
+                if (item.substring(0, getFirstCommaPosition(item)).equals(String.valueOf(x))) {
+                    sortedItems.add(item);
+                    break;
+                }
+            }
+            x ++;
+        }
+
+        checklistItems = sortedItems;
+    }
+
+    private void saveItems() {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putStringSet(CHECKLIST_ITEMS, new HashSet<>(checklistItems))
+                .apply();
+    }
+
+    void addItem(String item) {
+        checklistItems.add((checklistItems.size() + 1) + ", 0, " + item);
+        notifyItemInserted(checklistItems.size() + 1);
+
+        saveItems();
+    }
+}
--- app/src/main/java/org/cpsscifair/ssfcompanion/ChecklistFragment.java
+++ app/src/main/java/org/cpsscifair/ssfcompanion/ChecklistFragment.java
@@ -1,12 +1,19 @@
 package org.cpsscifair.ssfcompanion;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ChecklistFragment extends Fragment {
+
+    private static final String DIALOG_FRAGMENT = "dialog_fragment";
+
+    private ChecklistAdapter checklistAdapter;
 
     public ChecklistFragment() {
         // Required empty public constructor
@@ -20,6 +27,33 @@ public class ChecklistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checklist, container, false);
+        View v = inflater.inflate(R.layout.fragment_checklist, container, false);
+
+        setHasOptionsMenu(true);
+
+        final RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.checklist_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        checklistAdapter = new ChecklistAdapter(getContext());
+
+        recyclerView.setAdapter(checklistAdapter);
+
+        final FloatingActionButton floatingActionButton = (FloatingActionButton) v.findViewById(R.id.checklist_fab);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditTextDialogFragment.newInstance()
+                        .show(getActivity().getSupportFragmentManager(), DIALOG_FRAGMENT);
+            }
+        });
+
+        return v;
+    }
+
+    void addItem(String item) {
+        checklistAdapter.addItem(item);
     }
 }
