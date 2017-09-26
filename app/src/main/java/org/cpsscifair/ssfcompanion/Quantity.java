07e5712d842b231dda66ae79e0368b207805a2b9
Miles Krell <repos@mileskrell.com>
2017-09-25 20:44:30 -0500

    Made conversion helper methods return String and added an intermediate variable in preparation for another change

--- app/src/main/java/org/cpsscifair/ssfcompanion/Quantity.java
+++ app/src/main/java/org/cpsscifair/ssfcompanion/Quantity.java
@@ -35,81 +35,120 @@ class Quantity {
         switch (unit.getType()) {
             // Split into multiple methods for easier readability
             case DISTANCE:
-                result = convertDistance(amount, targetUnit).toString();
+                result = convertDistance(amount, targetUnit);
                 break;
             case MASS:
-                result = convertMass(amount, targetUnit).toString();
+                result = convertMass(amount, targetUnit);
                 break;
             case VOLUME:
-                result = convertVolume(amount, targetUnit).toString();
+                result = convertVolume(amount, targetUnit);
                 break;
         }
 
         return new Quantity(res, targetUnit, result);
     }
 
-    private BigDecimal convertDistance(BigDecimal amount, Unit targetUnit) {
+    private String convertDistance(BigDecimal amount, Unit targetUnit) {
         BigDecimal amountMeters = BigDecimal.ZERO;
         switch (unit) {
 //            case INCHES: amountMeters = amount.; break;
 //            case FEET: amountMeters = amount.; break;
 //            case YARDS: amountMeters = amount.; break;
 //            case MILES: amountMeters = amount.; break;
-            case MILLIMETERS: amountMeters = amount.divide(THOUSAND); break;
-            case CENTIMETERS: amountMeters = amount.divide(HUNDRED); break;
-            case METERS: amountMeters = amount; break;
-            case KILOMETERS: amountMeters = amount.multiply(THOUSAND); break;
+            case MILLIMETERS:
+                amountMeters = amount.divide(THOUSAND);
+                break;
+            case CENTIMETERS:
+                amountMeters = amount.divide(HUNDRED);
+                break;
+            case METERS:
+                amountMeters = amount;
+                break;
+            case KILOMETERS:
+                amountMeters = amount.multiply(THOUSAND);
+                break;
         }
+        BigDecimal amountFinal = BigDecimal.ZERO;
         switch (targetUnit) {
 //            case INCHES: return amountMeters.;
 //            case FEET: return amountMeters.;
 //            case YARDS: return amountMeters.;
 //            case MILES: return amountMeters.;
-            case MILLIMETERS: return amountMeters.multiply(THOUSAND);
-            case CENTIMETERS: return amountMeters.multiply(HUNDRED);
-            case METERS: return amountMeters;
-            case KILOMETERS: return amountMeters.divide(THOUSAND);
+            case MILLIMETERS:
+                amountFinal = amountMeters.multiply(THOUSAND);
+                break;
+            case CENTIMETERS:
+                amountFinal = amountMeters.multiply(HUNDRED);
+                break;
+            case METERS:
+                amountFinal = amountMeters;
+                break;
+            case KILOMETERS:
+                amountFinal = amountMeters.divide(THOUSAND);
+                break;
         }
-        return new BigDecimal(111);
+        return amountFinal.toString();
     }
 
-    private BigDecimal convertMass(BigDecimal amount, Unit targetUnit) {
+    private String convertMass(BigDecimal amount, Unit targetUnit) {
         BigDecimal amountGrams = BigDecimal.ZERO;
         switch (unit) {
 //            case OUNCES: amountGrams = amount.; break;
 //            case POUNDS: amountGrams = amount.; break;
-            case MILLIGRAMS: amountGrams = amount.divide(THOUSAND); break;
-            case GRAMS: amountGrams = amount; break;
-            case KILOGRAMS: amountGrams = amount.multiply(THOUSAND); break;
+            case MILLIGRAMS:
+                amountGrams = amount.divide(THOUSAND);
+                break;
+            case GRAMS:
+                amountGrams = amount;
+                break;
+            case KILOGRAMS:
+                amountGrams = amount.multiply(THOUSAND);
+                break;
         }
+        BigDecimal amountFinal = BigDecimal.ZERO;
         switch (targetUnit) {
 //            case OUNCES: return amountGrams.;
 //            case POUNDS: return amountGrams.;
-            case MILLIGRAMS: return amountGrams.multiply(THOUSAND);
-            case GRAMS: return amountGrams;
-            case KILOGRAMS: return amountGrams.divide(THOUSAND);
+            case MILLIGRAMS:
+                amountFinal = amountGrams.multiply(THOUSAND);
+                break;
+            case GRAMS:
+                amountFinal = amountGrams;
+                break;
+            case KILOGRAMS:
+                amountFinal = amountGrams.divide(THOUSAND);
+                break;
         }
-        return new BigDecimal(222);
+        return amountFinal.toString();
     }
 
-    private BigDecimal convertVolume(BigDecimal amount, Unit targetUnit) {
+    private String convertVolume(BigDecimal amount, Unit targetUnit) {
         BigDecimal amountLiters = BigDecimal.ZERO;
         switch (unit) {
 //            case FLUID_OUNCES: amountLiters = amount.; break;
 //            case PINTS: amountLiters = amount.; break;
 //            case QUARTS: amountLiters = amount.; break;
 //            case GALLONS: amountLiters = amount.; break;
-            case MILLILITERS: amountLiters = amount.divide(THOUSAND); break;
-            case LITERS: amountLiters = amount; break;
+            case MILLILITERS:
+                amountLiters = amount.divide(THOUSAND);
+                break;
+            case LITERS:
+                amountLiters = amount;
+                break;
         }
+        BigDecimal amountFinal = BigDecimal.ZERO;
         switch (targetUnit) {
 //            case FLUID_OUNCES: return amountLiters.;
 //            case PINTS: return amountLiters.;
 //            case QUARTS: return amountLiters.;
 //            case GALLONS: return amountLiters.;
-            case MILLILITERS: return amountLiters.multiply(THOUSAND);
-            case LITERS: return amountLiters;
+            case MILLILITERS:
+                amountFinal = amountLiters.multiply(THOUSAND);
+                break;
+            case LITERS:
+                amountFinal = amountLiters;
+                break;
         }
-        return new BigDecimal(333);
+        return amountFinal.toString();
     }
 }
